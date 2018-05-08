9e07b29a3d74dde77a4994a446c7cb025632add9
sophia.nesamoney <sophia.nesamoney@gmail.com>
2018-05-08 09:56:04 -0700

    Done

---
+++ TableViewDropDown/ExpandableHeaderView/Section`.swift
@@ -0,0 +1,9 @@
+//
+//  Section`.swift
+//  TableViewDropDown
+//
+//  Created by Sophia Nesamoney on 4/30/18.
+//  Copyright © 2018 Sophia Nesamoney. All rights reserved.
+//
+
+import Foundation
--- TableViewDropDown/ViewController.swift
+++ TableViewDropDown/ViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ExpandableHeaderViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sections = [
+        Section(genre: "Meeting 1",
+                movies: ["Purpose", "Tutor", "Date and Time"],
+                expanded: false ),
+        Section(genre: "Meeting 2",
+                movies: ["Purpose", "Tutor", "Date and Time"],
+                expanded: false),
+        Section(genre: "Meeting 3",
+                movies: ["Purpose", "Tutor", "Date and Time"],
+                expanded: false)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].movies.count
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if sections[indexPath.section].expanded {
+            return 44
+        } else {
+            return 0
+        }
+    }
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = ExpandableHeaderView()
+        header.customInit(title: sections[section].genre, section: section, delegate: self)
+        return header
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "labelCell")!
+        cell.textLabel?.text = sections[indexPath.section].movies[indexPath.row]
+        return cell
+    }
+    func toggleSection(header: ExpandableHeaderView, section: Int) {
+        sections[section].expanded = !sections[section].expanded
+        
+        tableView.beginUpdates()
+        for i in 0 ..< sections[section].movies.count {
+            tableView.reloadRows(at: [IndexPath(row: i, section: section)], with: .automatic)
+        }
+        tableView.endUpdates()
+    }
 }
 
